b6ce01aa6bd4713eee104c6e2eeeeea73a60fd5c
SegunPc <eeomo@yahoo.com>
2023-05-06 00:43:41 +0100

    fix 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -3,12 +3,12 @@
 """
 export data to csv
 """
-from request import get
+from requests import get
 from sys import argv
 import csv
 
 
-if __name__="__main__":
+if __name__=="__main__":
     # making request to get todo data and user data
     response = get('https://jsonplaceholder.typicode.com/todos/').json()
     response2= get('https://jsonplaceholder.typicode.com/users').json()
@@ -18,18 +18,16 @@ if __name__="__main__":
         if user['id'] == int(argv[1]):
             employee_name = user['name']
 
-    with open(argv[1] + '.csv', 'w', newline='') as file
-    writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+    with open(argv[1] + '.csv', 'w', newline='') as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
 
-    for i in response:
+        for i in response:
+            row = []
+            if i['userId'] == int(argv[1]):
+                row.append(i['userId'])
+                row.append(employee_name)
+                row.append(i['completed'])
+                row.append(i['title'])
 
-        row = []
-
-        if i['userId'] == int(argv[1]):
-             row.append(i['userId'])
-             row.append(employee)
-             row.append(i['completed'])
-             row.append(i['title'])
-
-             writer.writerow(row)
+                writer.writerow(row)
 
