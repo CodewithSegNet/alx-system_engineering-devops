831df6394873faafef257b7b7eb089e8242e8d9c
SegunPc <eeomo@yahoo.com>
2023-05-06 00:45:57 +0100

    fix

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,33 +1,39 @@
 #!/usr/bin/python3
 
-"""
-export data to csv
-"""
-from requests import get
-from sys import argv
 import csv
+import requests
+import sys
 
+if __name__ == "__main__":
+    response = requests.get('https://jsonplaceholder.typicode.com/todos/')
+    data = response.json()
+    response2 = requests.get('https://jsonplaceholder.typicode.com/users')
+    data2 = response2.json()
 
-if __name__=="__main__":
-    # making request to get todo data and user data
-    response = get('https://jsonplaceholder.typicode.com/todos/').json()
-    response2= get('https://jsonplaceholder.typicode.com/users').json()
+    # Get employee name and user ID
+    employee_name = None
+    user_id = int(sys.argv[1])
+    for user in data2:
+        if user['id'] == user_id:
+            employee_name = user['id']
+            break
 
-    # getting user user id and name
-    for user in response2:
-        if user['id'] == int(argv[1]):
-            employee_name = user['name']
+    if employee_name is None:
+        print(f"User ID {user_id} not found")
+        sys.exit(1)
 
-    with open(argv[1] + '.csv', 'w', newline='') as file:
-        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+    # Get tasks owned by the employee
+    tasks = []
+    for task in data:
+        if task['userId'] == user_id:
+            tasks.append(task)
 
-        for i in response:
-            row = []
-            if i['userId'] == int(argv[1]):
-                row.append(i['userId'])
-                row.append(employee_name)
-                row.append(i['completed'])
-                row.append(i['title'])
+    # Export data to CSV
+    filename = f"{user_id}.csv"
+    with open(filename, mode='w', newline='') as csv_file:
+        writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE'])
+        for task in tasks:
+            writer.writerow([task['userId'], employee_name, task['completed'], task['title']])
 
-                writer.writerow(row)
-
+    print(f"Data written to {filename}")
