1e7453dbdd61cd8a4d7a3321d922a84c7ca15054
SegunPc <eeomo@yahoo.com>
2023-05-06 00:27:18 +0100

    fix 1-export

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,39 +1,35 @@
 #!/usr/bin/python3
 
+"""
+export data to csv
+"""
+from request import get
+from sys import argv
 import csv
-import requests
-import sys
 
-if __name__ == "__main__":
-    response = requests.get('https://jsonplaceholder.typicode.com/todos/')
-    data = response.json()
-    response2 = requests.get('https://jsonplaceholder.typicode.com/users')
-    data2 = response2.json()
 
-    # Get employee name and user ID
-    employee_name = None
-    user_id = int(sys.argv[1])
-    for user in data2:
-        if user['id'] == user_id:
-            employee_name = user['id']
-            break
+if __name__="__main__":
+    # making request to get todo data and user data
+    response = get('https://jsonplaceholder.typicode.com/todos/').json()
+    response2= get('https://jsonplaceholder.typicode.com/users').json()
 
-    if employee_name is None:
-        print(f"User ID {user_id} not found")
-        sys.exit(1)
+    # getting user user id and name
+    for user in response2:
+        if user['id'] == int(argv[1]):
+            employee_name = user['name']
 
-    # Get tasks owned by the employee
-    tasks = []
-    for task in data:
-        if task['userId'] == user_id:
-            tasks.append(task)
+    with open(argv[1] + '.csv', 'w', newline='') as file
+    writer = csv.writer(file, quoting=csv.QUOTE_ALL)
 
-    # Export data to CSV
-    filename = f"{user_id}.csv"
-    with open(filename, mode='w', newline='') as csv_file:
-        writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        writer.writerow(['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE'])
-        for task in tasks:
-            writer.writerow([task['userId'], employee_name, task['completed'], task['title']])
+    for i in response:
 
-    print(f"Data written to {filename}")
+        row = []
+
+        if i['userId'] == int(argv[1]):
+             row.append(i['userId'])
+             row.append(employee)
+             row.append(i['completed'])
+             row.append(i['title'])
+
+             writer.writerow(row)
+
