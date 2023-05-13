492a5e45cfe11dd89536b5a8dae54da5f6fa7892
segunPc <eeomo@yahoo.com>
2023-05-13 22:24:44 +0100

    0-subs.py

---
+++ 0x16-api_advanced/test/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" a function that queries the Reddit API and \
+        returns the number of subscribers"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Queries the Reddit API and returns the number of subscribers."""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/89.0.4389.82 Safari/537.36"
+    }
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        return response.json().get("data").get("subscribers")
+
+    return 0
