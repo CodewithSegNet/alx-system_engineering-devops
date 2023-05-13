3efb489852748e41e5485c6834afde2e004b048b
segunPc <eeomo@yahoo.com>
2023-05-13 23:09:39 +0100

    fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-"""Queries the Reddit API and returns the number of subscribers"""
+"""Exporting CSV files"""
 import json
 import requests
+import sys
 
 
 def number_of_subscribers(subreddit):
     """Queries the Reddit API and returns the number of subscribers."""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
         "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) "
         "AppleWebKit/537.36 (KHTML, like Gecko) "
         "Chrome/89.0.4389.82 Safari/537.36"
     }
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
 
     response = requests.get(url, headers=headers, allow_redirects=False)
 
