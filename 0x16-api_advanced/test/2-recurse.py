0e013c1b141a6f7974ae6cd91f8c808df766845f
segunPc <eeomo@yahoo.com>
2023-05-15 10:05:47 +0100

    2-recurse.py

---
+++ 0x16-api_advanced/test/2-recurse.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" a recursive function that queries and
+returns a list containing the titles of
+all hot articles"""
+
+import json
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=""):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) "
+                      "AppleWebKit/537.36 (KHTML, like Gecko) "
+                      "Chrome/89.0.4389.82 Safari/537.36"
+    }
+
+    params = {"after": after, "limit": 100}
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 200:
+        results = response.json().get("data")
+        after = results.get("after")
+        for entry in results.get("children"):
+            hot_list.append(entry.get("data").get("title"))
+            if after:
+                return recurse(subreddit, hot_list, after)
+            return hot_list
+    return None
