6f7e3594c27291ffecd9093349788771304164cd
Smobile <eeomo@yahoo.com>
2023-05-18 17:27:51 +0000

    1-user_limit.pp fix

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,15 +1,8 @@
-# A puppet manifest that change the user limit
-file { '/etc/security/limits.conf':
-  ensure => file,
-  require => Exec['change_value_to_50'],
+# A puppet manifest to change user limit
+exec { 'change_value_to_50' :
+  command => "/bin/sed -i 's/5/50/g' /etc/security/limits.conf",
 }
 
-exec { 'change_value_to_50':
-  command     => "/bin/sed -i 's/5/50/g' /etc/security/limits.conf",
-  refreshonly => true,
-}
-
-exec { 'change_value_to_40':
-  command     => "/bin/sed -i 's/4/40/g' /etc/security/limits.conf",
-  refreshonly => true,
+exec { 'change_value_to_40' :
+  command => "/bin/sed -i 's/4/40/g' /etc/security/limits.conf",
 }
