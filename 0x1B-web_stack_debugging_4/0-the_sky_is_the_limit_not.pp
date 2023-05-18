fea122bab8aa72be599cd7f529e998f05b206f8a
segunPc <eeomo@yahoo.com>
2023-05-18 15:13:56 +0100

    0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,14 +1,7 @@
 # It allows you to simulate HTTP requests to a web server. 
-# In this case, 
 # I will make 2000 requests to my server with 100 requests at a time.
 
-#file_line{'ulimit_option':
-#   pathi => '/etc/default/nginx',
-#    line  => 'ULIMIT="-n 2049"\n',
-#    match => '^ULIMIT='
-#}
-
-file{'/etc/default/nginx':
+file{ '/etc/default/nginx':
     ensure  => file,
     content => "ULIMIT='-n 2049'\n"
 }
